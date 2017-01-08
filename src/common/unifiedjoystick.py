a8aa0a706e452960ab4cffaba27511d8d2b56166
DrWateryCat <DrWateryCat@gmail.com>
2017-01-08 15:29:45 -0500

    Updated UnifiedJoystick to accept any number of joysticks

--- src/common/unifiedjoystick.py
+++ src/common/unifiedjoystick.py
@@ -8,33 +8,16 @@ class UnifiedJoystick(object):
     A class to allow for unified output of buttons from 2+ joysticks
     '''
     
-    def __init__(self, *args, **kwargs):
-        """
-        Keyword args:
+    def __init__(self, *joysticks):
+        self.joysticks = joysticks
         
-        leftJoystick
-        rightJoystick
-        
-        Positional args:
-        leftJoystick, rightJoystick
-        
-        TODO: Add more than 2 joysticks, if that would ever come up
-        """
-        
-        #Keyword args
-        self.leftJoystick = kwargs.pop('leftJoystick', None)
-        self.rightJoystick = kwargs.pop('rightJoystick', None)
-        
-        #Posititional args
-        if len(args) == 2:
-            self.leftJoystick = args[0]
-            self.rightJoystick = args[1]
-        else:
-            raise ValueError("Either not enough inputs, or too many, try only using 2")
         
     def getButton(self, button):
         """
         Calls getRawButton, then or's the output
         """
         
-        return (self.leftJoystick.getRawButton(button) or self.rightJoystick.getRawButton(button))
+        ret = False
+        for i in self.joysticks:
+            ret |= i.getRawButton(button)
+        return ret
